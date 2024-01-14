3b2d52dbf7718bdefb00566f09a663887925c81c
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-14 17:10:33 +0100

    Update changes

--- src/components/Contributor.jsx
+++
@@ -1,9 +0,0 @@
-export default function Owner(){
-    return(
-        <div class="footer">
-            by <a href="https://github.com/ndabdulsalaam" target="_blank">
-            Nurudeen Abdulsalaam
-            </a>
-        </div>    
-    )
-}
